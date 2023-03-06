4e54692c3537f49b6227e58335a4fc03a54f3b74
Kshitij Todkar <kshitijtodkar4826@gmail.com>
2023-03-06 22:11:12 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Board from "./components/Board/Board";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <span>Tic Tac Toe</span>
+       <Board/>
+    </div>
+  );
+}
+
+export default App;
